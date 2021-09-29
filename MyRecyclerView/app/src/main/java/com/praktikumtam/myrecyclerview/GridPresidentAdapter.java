44e874abdeea856277df7da96bee73cb01e655d0
ryecries <you@example.com>
2021-09-29 23:48:28 +0700

    recycler

---
+++ MyRecyclerView/app/src/main/java/com/praktikumtam/myrecyclerview/GridPresidentAdapter.java
@@ -0,0 +1,60 @@
+package com.praktikumtam.myrecyclerview;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+
+import java.util.ArrayList;
+
+public class GridPresidentAdapter extends RecyclerView.Adapter<GridPresidentAdapter.GridViewHolder> {
+    public GridPresidentAdapter(Context context) {
+        this.context = context;
+    }
+
+    private Context context;
+
+    public ArrayList<President> getListPresident() {
+        return listPresident;
+    }
+
+    public void setListPresident(ArrayList<President> listPresident) {
+        this.listPresident = listPresident;
+    }
+
+    private ArrayList<President> listPresident;
+
+    @NonNull
+    @Override
+    public GridPresidentAdapter.GridViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_grid_president, parent, false);
+        return new GridViewHolder(view);
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull GridPresidentAdapter.GridViewHolder holder, int position) {
+        Glide.with(context).load(getListPresident().get(position).getPhoto()).apply(new RequestOptions().override(350, 550)).into(holder.imgPhoto);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return getListPresident().size();
+    }
+
+    class GridViewHolder extends RecyclerView.ViewHolder {
+        ImageView imgPhoto;
+        GridViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imgPhoto = itemView.findViewById(R.id.img_item_photo);
+        }
+    }
+}
